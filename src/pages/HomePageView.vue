bb36837cf6b5b95f3b1cc6065e85d562ebd9eae0
Sagrefukom <Sagrefukom@gmail.com>
2024-03-10 19:55:45 +0200

    Created and configured the project, added typography to project

---
+++ src/pages/HomePageView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Text } from '@/shared/ui/typography'
+</script>
+
+<template>
+  <main>
+    <Text tagName="p" size="xs">llala</Text>
+    <Text tagName="'span'" bold>llala</Text>
+    <Text tagName="'h1'">llala</Text>
+    <Text tagName="'h2'">llala</Text>
+    <Text tagName="'h3'">llala</Text>
+    <Text tagName="'h4'">llala</Text>
+    <Text tagName="'h5'">llala</Text>
+  </main>
+</template>
---
+++ src/shared/ui/typography/Text/TextComponent.vue
@@ -0,0 +1,101 @@
+<script lang="ts" setup>
+interface Text {
+  tagName: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  size?: 'xs' | 's' | 'm' | 'l' | 'xl'
+  bold?: boolean
+}
+
+const props = defineProps<Text>()
+</script>
+
+<template>
+  <component
+    :is="props.tagName"
+    class="typography"
+    :class="[`tag--${tagName}`, size ? `size--${size}` : '', bold ? 'typography--bold' : '']"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<style scoped lang="scss">
+.typography {
+  font-style: normal;
+  line-height: 150%;
+
+  &--bold {
+    font-weight: 700;
+  }
+}
+
+.tag {
+  &--h1 {
+    font-size: 64px;
+  }
+  &--h2 {
+    font-size: 48px;
+  }
+  &--h3 {
+    font-size: 36px;
+  }
+  &--h4 {
+    font-size: 24px;
+  }
+  &--h5 {
+    font-size: 18px;
+  }
+
+  @media (max-width: 768px) {
+    &--h1 {
+      font-size: 48px;
+    }
+    &--h2 {
+      font-size: 36px;
+    }
+    &--h3 {
+      font-size: 24px;
+    }
+    &--h4 {
+      font-size: 20px;
+    }
+    &--h5 {
+      font-size: 16px;
+    }
+  }
+}
+.size {
+  &--xs {
+    font-size: 12px;
+  }
+  &--s {
+    font-size: 16px;
+  }
+  &--m {
+    font-size: 18px;
+  }
+  &--l {
+    font-size: 24px;
+  }
+  &--xl {
+    font-size: 36px;
+  }
+
+  @media (max-width: 768px) {
+    &--xs {
+      font-size: 8px;
+    }
+    &--s {
+      font-size: 12px;
+    }
+    &--m {
+      font-size: 14px;
+    }
+    &--l {
+      font-size: 20px;
+    }
+    &--xl {
+      font-size: 32px;
+    }
+  }
+}
+</style>
